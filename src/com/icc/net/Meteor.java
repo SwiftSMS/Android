16c080c6fe8d206ae945fe89ffdafa63b376e3f9
Sean Dunne <sean.dunne1988@gmail.com>
2013-10-03 22:12:16 +0100

    Updated the networking to the recommended HttpUrlConnection

--- src/com/icc/net/Meteor.java
+++ src/com/icc/net/Meteor.java
@@ -2,40 +2,37 @@ package com.icc.net;
 
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
-
-import org.apache.http.Header;
-import org.apache.http.HttpEntity;
-import org.apache.http.HttpResponse;
-import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.client.methods.HttpUriRequest;
-import org.apache.http.impl.client.DefaultHttpClient;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class Meteor {
 
 	public String login(final String username, final String password) {
-		final HttpUriRequest request = new HttpPost("http://www.mymeteor.ie");
-		final HttpClient client = new DefaultHttpClient();
 		final StringBuilder result = new StringBuilder();
-
 		try {
-			final HttpResponse response = client.execute(request);
-			final Header[] headers = response.getAllHeaders();
-			for (final Header header : headers) {
-				result.append(header.getName());
-				result.append(" = ");
-				result.append(header.getValue());
-			}
+			final URL url = new URL("https://www.mymeteor.ie/go/mymeteor-login-manager");
+			final HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+			connection.setDoOutput(true);
+			connection.setRequestMethod("POST");
 
-			final HttpEntity entity = response.getEntity();
-			final BufferedReader is = new BufferedReader(new InputStreamReader(entity.getContent()));
-			String line;
-			while ((line = is.readLine()) != null) {
-				result.append(line);
-			}
-		} catch (final ClientProtocolException e) {
+			final Writer writer = new OutputStreamWriter(connection.getOutputStream());
+			writer.write("username=" + username);
+			writer.write("&userpass=" + password);
+			writer.write("&login=");
+			writer.write("&returnTo=/");
+			writer.close();
+
+			result.append(connection.getResponseCode());
+			result.append(connection.getHeaderFields().toString());
+			// result.append(this.readStream(connection.getErrorStream()));
+			result.append(this.readStream(connection.getInputStream()));
+
+		} catch (final MalformedURLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} catch (final IOException e) {
@@ -43,6 +40,16 @@ public class Meteor {
 			e.printStackTrace();
 		}
 
+		return result.toString();
+	}
+
+	private String readStream(final InputStream is) throws IOException {
+		final StringBuilder result = new StringBuilder();
+		final BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+		String line;
+		while ((line = reader.readLine()) != null) {
+			result.append(line);
+		}
 		return result.toString();
 	}
 }
