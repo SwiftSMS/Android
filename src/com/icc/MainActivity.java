471b3e6661e02e5a385fe280101f1392a7950eab
Sean Dunne <sean.dunne1988@gmail.com>
2013-10-08 23:23:33 +0100

    Added send button functionality

--- src/com/icc/MainActivity.java
+++ src/com/icc/MainActivity.java
@@ -1,12 +1,15 @@
 package com.icc;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.AlertDialog.Builder;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.TextView;
+import android.view.View;
+import android.widget.EditText;
 
 import com.icc.acc.Account;
 import com.icc.acc.Network;
@@ -16,23 +19,37 @@ import com.icc.view.acc.AddAccountActivity;
 
 public class MainActivity extends Activity {
 
+	private EditText messageEditText;
+	private EditText recipientsEditText;
+
 	@Override
 	protected void onCreate(final Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		this.setContentView(R.layout.activity_main);
 
+		this.messageEditText = (EditText) this.findViewById(R.id.text_compose_message);
+		this.recipientsEditText = (EditText) this.findViewById(R.id.text_compose_recipients);
+	}
+
+	public void sendMessage(final View view) {
 		new AsyncTask<String, Integer, String>() {
 			@Override
 			protected String doInBackground(final String... params) {
-				final Account vodaAccount = new Account("user", "Work", "pass", Network.VODAFONE);
-				final Operator operator = new Meteor(vodaAccount);
-				return operator.login();
+				final Account account = new Account("user", "My Meteor", "pass", Network.METEOR);
+				final Operator operator = new Meteor(account);
+				operator.login();
+
+				final String message = MainActivity.this.messageEditText.getText().toString();
+				final String recipients = MainActivity.this.recipientsEditText.getText().toString();
+				return operator.send(recipients, message);
 			}
 
 			@Override
 			protected void onPostExecute(final String result) {
-				final TextView view = (TextView) MainActivity.this.findViewById(R.id.text_compose_message);
-				view.setText(result);
+				final Builder builder = new AlertDialog.Builder(MainActivity.this);
+				builder.setMessage(result);
+				builder.setTitle("Server Message");
+				builder.show();
 			}
 		}.execute();
 	}
