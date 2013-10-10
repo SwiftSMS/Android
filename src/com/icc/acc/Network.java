fb2257c0d5577b6e386e136cc87c8bc73f4f61cf
Rob Powell <rob.p.tec@gmail.com>
2013-10-10 15:25:06 +0100

    Added Network selection before Account Input, pretty sexy i must say :)

--- src/com/icc/acc/Network.java
+++ src/com/icc/acc/Network.java
@@ -1,11 +1,22 @@
 package com.icc.acc;
 
 /**
- * Simple enum to hold the current operators ICC will support.
+ * Simple enum to hold the current Network operators ICC will support.
  * 
  * @author Rob Powell
- * @version 1.0
+ * @version 1.1
  */
 public enum Network {
-	O2, METEOR, VODAFONE, TESCO, EMOBILE, THREE
+	O2("O2"), METEOR("Meteor"), VODAFONE("Vodafone"), TESCO("Tesco"), EMOBILE("EMobile"), THREE("Three");
+
+    private final String network;
+
+    private Network(String network) {
+        this.network = network;
+    }
+
+    @Override
+    public String toString() {
+        return this.network;
+    }
 }
--- src/com/icc/view/acc/AddAccountActivity.java
+++ src/com/icc/view/acc/AddAccountActivity.java
@@ -1,15 +1,16 @@
 package com.icc.view.acc;
 
 import android.app.Activity;
+import android.app.Dialog;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.Spinner;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
-
 import com.icc.InternalString;
 import com.icc.R;
 import com.icc.acc.Account;
@@ -23,14 +24,14 @@ import java.util.List;
  * Activity to handle adding a new operator account to ICC
  * 
  * @author Rob Powell
- * @version 1.0
+ * @version 1.3
  */
 public class AddAccountActivity extends Activity {
 
 	private IAccountDatabase accountDatabase;
-	private TextView textAccNumber, textAccName, textAccPassword;
-    private Spinner networkSpinner;
+	private TextView textAccNumber, textAccName, textAccPassword, textViewNetwork;
     private SharedPreferences preferences;
+    private Network selectedNetwork = null;
 
 	@Override
 	public void onCreate(final Bundle savedInstanceState) {
@@ -41,22 +42,37 @@ public class AddAccountActivity extends Activity {
         AddAccountActivity.this.textAccName = (TextView)this.findViewById(R.id.text_acc_name);
         AddAccountActivity.this.textAccNumber = (TextView) this.findViewById(R.id.text_acc_number);
         AddAccountActivity.this.textAccPassword = (TextView) this.findViewById(R.id.text_acc_password);
+        AddAccountActivity.this.textViewNetwork = (TextView) this.findViewById(R.id.text_selected_network);
 	}
 
 	@Override
 	protected void onResume() {
 
-        AddAccountActivity.this.networkSpinner = (Spinner)findViewById(R.id.spinner_acc_network);
-        AddAccountActivity.this.networkSpinner.setAdapter(getNetworkAdapter());
-
         AddAccountActivity.this.accountDatabase = AccountDataSource.getInstance(this);
-
+        handleNetworkSelection();
 		super.onResume();
 	}
 
-    private ArrayAdapter getNetworkAdapter() {
+    private void handleNetworkSelection() {
 
-        return new ArrayAdapter(this, android.R.layout.simple_list_item_1, Network.values());
+        final Dialog dialog = new Dialog(AddAccountActivity.this);
+        final ListView listView = new ListView(this);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                selectedNetwork = Network.values()[i];
+                textViewNetwork.setText(selectedNetwork +" Network");
+                dialog.dismiss();
+            }
+        });
+        listView.setAdapter(getNetworkAdapter());
+        dialog.setTitle("Select Network");
+        dialog.setContentView(listView);
+        dialog.show();
+    }
+
+    private ArrayAdapter<Network> getNetworkAdapter() {
+        return new ArrayAdapter<Network>(this, android.R.layout.simple_list_item_1, Network.values());
     }
 
     public void addAccount(final View view) {
@@ -64,15 +80,14 @@ public class AddAccountActivity extends Activity {
         final String number = AddAccountActivity.this.textAccNumber.getText().toString();
         final String accountName = AddAccountActivity.this.textAccName.getText().toString();
         final String password = AddAccountActivity.this.textAccPassword.getText().toString();
-        final Network network = (Network)AddAccountActivity.this.networkSpinner.getSelectedItem();
 
-        final Account account = new Account(number, accountName, password, network);
+        final Account account = new Account(number, accountName, password, selectedNetwork);
 
         final boolean successfullyAdded = this.accountDatabase.addAccount(account);
 
         if(successfullyAdded) {
-            Toast.makeText(this,"Accounted Added",Toast.LENGTH_SHORT).show();
-            preferences.edit().putInt(InternalString.ACCOUNT_LATEST, getLatestAccount().getId());
+            Toast.makeText(this,"Account Added",Toast.LENGTH_SHORT).show();
+            preferences.edit().putInt(InternalString.ACCOUNT_LATEST, getLatestAccount().getId()).commit();
         }
     }
 
