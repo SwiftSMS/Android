ee02628be62fe11f79a3b3c3c74bc4efb07a99ef
Rob Powell <rob.p.tec@gmail.com>
2013-10-10 11:22:59 +0100

    Account now saving, working on prefs to save active account :)

---
+++ src/com/icc/InternalString.java
@@ -0,0 +1,10 @@
+package com.icc;
+
+/**
+ * @author Rob Powell
+ */
+public class InternalString {
+
+    public static final String PREFS_KEY = "com.icc.app.prefs";
+    public static final String ACCOUNT_LATEST = "latestAccount";
+}
--- src/com/icc/view/acc/AddAccountActivity.java
+++ src/com/icc/view/acc/AddAccountActivity.java
@@ -1,16 +1,23 @@
 package com.icc.view.acc;
 
-import java.util.List;
-
 import android.app.Activity;
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.icc.InternalString;
 import com.icc.R;
 import com.icc.acc.Account;
 import com.icc.acc.Network;
 import com.icc.db.AccountDataSource;
 import com.icc.db.IAccountDatabase;
+
+import java.util.List;
 
 /**
  * Activity to handle adding a new operator account to ICC
@@ -21,30 +28,68 @@ import com.icc.db.IAccountDatabase;
 public class AddAccountActivity extends Activity {
 
 	private IAccountDatabase accountDatabase;
-	private TextView textView;
+	private TextView textAccNumber, textAccName, textAccPassword;
+    private Spinner networkSpinner;
+    private SharedPreferences preferences;
 
 	@Override
 	public void onCreate(final Bundle savedInstanceState) {
+        preferences = this.getSharedPreferences(InternalString.PREFS_KEY, Context.MODE_PRIVATE);
 		super.onCreate(savedInstanceState);
 		this.setContentView(R.layout.add_account_activity);
 
-		this.textView = (TextView) this.findViewById(R.id.main_text);
+        AddAccountActivity.this.textAccName = (TextView)this.findViewById(R.id.text_acc_name);
+        AddAccountActivity.this.textAccNumber = (TextView) this.findViewById(R.id.text_acc_number);
+        AddAccountActivity.this.textAccPassword = (TextView) this.findViewById(R.id.text_acc_password);
 	}
 
 	@Override
 	protected void onResume() {
 
-		this.accountDatabase = AccountDataSource.getInstance(this);
+        AddAccountActivity.this.networkSpinner = (Spinner)findViewById(R.id.spinner_acc_network);
+        AddAccountActivity.this.networkSpinner.setAdapter(getNetworkAdapter());
 
-		final Account account = new Account("00353862101112", "My O2 Account", "hello", Network.O2);
-
-		final boolean addedSuccessfully = this.accountDatabase.addAccount(account);
-
-		if (addedSuccessfully) {
-			final List<Account> accountList = this.accountDatabase.getAllAccounts();
-			this.textView.setText(accountList.get(0).getAccountName());
-		}
+        AddAccountActivity.this.accountDatabase = AccountDataSource.getInstance(this);
 
 		super.onResume();
 	}
+
+    private ArrayAdapter getNetworkAdapter() {
+
+        return new ArrayAdapter(this, android.R.layout.simple_list_item_1, Network.values());
+    }
+
+    public void addAccount(final View view) {
+
+        final String number = AddAccountActivity.this.textAccNumber.getText().toString();
+        final String accountName = AddAccountActivity.this.textAccName.getText().toString();
+        final String password = AddAccountActivity.this.textAccPassword.getText().toString();
+        final Network network = (Network)AddAccountActivity.this.networkSpinner.getSelectedItem();
+
+        final Account account = new Account(number, accountName, password, network);
+
+        final boolean successfullyAdded = this.accountDatabase.addAccount(account);
+
+        if(successfullyAdded) {
+            Toast.makeText(this,"Accounted Added",Toast.LENGTH_SHORT).show();
+            preferences.edit().putInt(InternalString.ACCOUNT_LATEST, getLatestAccount().getId());
+        }
+    }
+
+    private Account getLatestAccount() {
+
+        final List<Account> accounts = accountDatabase.getAllAccounts();
+
+        Account latestAccount = null;
+        long createdTime = 0;
+
+        for(Account account : accounts) {
+            if(account.getTimeStamp() > createdTime){
+                latestAccount = account;
+                createdTime = account.getTimeStamp();
+            }
+        }
+
+        return latestAccount;
+    }
 }
