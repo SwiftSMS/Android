572d3a16581a192c885f11e8608fdd05648e20f4
Sean Dunne <sean.dunne1988@gmail.com>
2013-10-15 23:21:05 +0100

    Added a progress bar when sending message

--- src/com/icc/MainActivity.java
+++ src/com/icc/MainActivity.java
@@ -12,6 +12,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 
 import com.icc.acc.Account;
 import com.icc.db.AccountDataSource;
@@ -26,6 +27,7 @@ public class MainActivity extends Activity {
 	private EditText recipientsEditText;
 	private IAccountDatabase accountDatabase;
 	private SharedPreferences preferences;
+	private ProgressBar progressBar;
 
 	@Override
 	protected void onCreate(final Bundle savedInstanceState) {
@@ -36,30 +38,46 @@ public class MainActivity extends Activity {
 		this.accountDatabase = AccountDataSource.getInstance(this);
 
 		this.messageEditText = (EditText) this.findViewById(R.id.text_compose_message);
+		this.progressBar = (ProgressBar) this.findViewById(R.id.progressbar_compose);
 		this.recipientsEditText = (EditText) this.findViewById(R.id.text_compose_recipients);
 	}
 
 	public void sendMessage(final View view) {
 		new AsyncTask<String, Integer, String>() {
 			@Override
+			protected void onPreExecute() {
+				MainActivity.this.progressBar.setVisibility(View.VISIBLE);
+			}
+
+			@Override
 			protected String doInBackground(final String... params) {
 				final int accountId = MainActivity.this.preferences.getInt(InternalString.LATEST_ACCOUNT, -1);
 				final Account account = MainActivity.this.accountDatabase.getAccountById(accountId);
 				// final Account account = new Account("user", "My Meteor", "pass", Network.METEOR);
 				final Operator operator = new Meteor(account);
+				this.publishProgress(1);
 				operator.login();
+				this.publishProgress(2);
 
 				final String message = MainActivity.this.messageEditText.getText().toString();
 				final String recipients = MainActivity.this.recipientsEditText.getText().toString();
+				this.publishProgress(3);
 				return operator.send(recipients, message);
 			}
 
 			@Override
+			protected void onProgressUpdate(final Integer... values) {
+				MainActivity.this.progressBar.setProgress(values[0]);
+			}
+
+			@Override
 			protected void onPostExecute(final String result) {
+				this.publishProgress(4);
 				final Builder builder = new AlertDialog.Builder(MainActivity.this);
 				builder.setMessage(result);
 				builder.setTitle("Server Message");
 				builder.show();
+				MainActivity.this.progressBar.setVisibility(View.GONE);
 			}
 		}.execute();
 	}
