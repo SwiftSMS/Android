0b7b3cb76833f8e0804c61b9ec9711b434d1fe12
Seán Dunne <sean.dunne@ericsson.com>
2013-10-16 11:32:45 +0100

    Added Get remaining SMS count & Cleaned up strings in Meteor

--- src/com/icc/net/Meteor.java
+++ src/com/icc/net/Meteor.java
@@ -1,68 +1,73 @@
 package com.icc.net;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import com.icc.acc.Account;
 
 public class Meteor extends Operator {
+
+	private static final String LOGIN_SUCCESS_TEXT = "Log Out";
+	private static final String LOGIN_URL = "https://www.mymeteor.ie/go/mymeteor-login-manager";
+	private static final String POST_ADD = "add";
+	private static final String POST_AJAX_REQUEST = "ajaxRequest";
+	private static final String POST_MESSAGE_TEXT = "messageText";
+	private static final String POST_PASSWORD = "userpass";
+	private static final String POST_REMOVE = "remove";
+	private static final String POST_USERNAME = "username";
+	private static final String POST_VALUE_ADD_RECIPIENT = "addEnteredMSISDNs";
+	private static final String POST_VALUE_NO_ID = "0%7C";
+	private static final String POST_VALUE_NONE = "-";
+	private static final String POST_VALUE_SEND_SMS = "sendSMS";
+	private static final String REMAINING_SMS_URL = "https://www.mymeteor.ie/cfusion/meteor/Meteor_REST/service/freeSMS";
+	private static final String SMS_URL = "https://www.mymeteor.ie/mymeteorapi/index.cfm?event=smsAjax";
 
 	public Meteor(final Account account) {
 		super(account);
 	}
 
-	/**
-	 * <pre>
-	 * Steps to send
-	 * 	1. Get session cookie
-	 * 		- https://www.mymeteor.ie/go/mymeteor-login-manager
-	 * 		  POST
-	 * 			username=phone
-	 * 			userpass=pin
-	 * 			login=
-	 * 			returnTo=/
-	 * 		- Session cookie should be in the returned headers
-	 * 		  Example
-	 * 			CFID=35626768
-	 * 			CFTOKEN=13595949
-	 * 	2. Add recipients
-	 * 		- https://www.mymeteor.ie/mymeteorapi/index.cfm?event=smsAjax&CFID=35626768&CFTOKEN=13595949&func=addEnteredMsisdns
-	 * 		  POST (i think)
-	 * 			ajaxRequest=addEnteredMSISDNs
-	 * 			remove=-
-	 * 			add=NUMBER
-	 * 		- Form to replicate the adding
-	 * 			forms/meteor_addrecipient.html
-	 * 	3. Send message
-	 * 		- https://www.mymeteor.ie/mymeteorapi/index.cfm?event=smsAjax&CFID=35626768&CFTOKEN=13595949&func=sendSMS
-	 * 		  POST (i think)
-	 * 			ajaxRequest=sendSMS
-	 * 			messageText=MESSAGE
-	 * 		- Form to replicate the adding
-	 * 			forms/meteor_send.html
-	 * </pre>
-	 */
-
 	@Override
 	public boolean login() {
-		final ConnectionManager loginManager = new ConnectionManager("https://www.mymeteor.ie/go/mymeteor-login-manager");
-		loginManager.addPostHeader("username", this.getAccount().getMobileNumber());
-		loginManager.addPostHeader("userpass", this.getAccount().getPassword());
+		final ConnectionManager loginManager = new ConnectionManager(LOGIN_URL);
+		loginManager.addPostHeader(POST_USERNAME, this.getAccount().getMobileNumber());
+		loginManager.addPostHeader(POST_PASSWORD, this.getAccount().getPassword());
 		final String loginHtml = loginManager.doConnection();
 
-		return loginHtml.contains("Sign out");
+		return loginHtml.contains(LOGIN_SUCCESS_TEXT);
+	}
+
+	public int getRemainingSMS() {
+		final ConnectionManager manager = new ConnectionManager(REMAINING_SMS_URL);
+		final String smsHtml = manager.doConnection();
+
+		try {
+			final JSONObject smsJson = new JSONObject(smsHtml);
+			return smsJson.getInt("remainingFreeSMS");
+		} catch (final JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		return 0;
 	}
 
 	@Override
 	public String send(final String recipient, final String message) {
-		final String addUrl = "https://www.mymeteor.ie/mymeteorapi/index.cfm?event=smsAjax";
-		final ConnectionManager addManager = new ConnectionManager(addUrl);
-		addManager.addPostHeader("ajaxRequest", "addEnteredMSISDNs");
-		addManager.addPostHeader("remove", "-");
-		addManager.addPostHeader("add", "0%7C" + recipient);
-		addManager.doConnection();
+		this.addRecipient(recipient);
 
-		final String sendUrl = "https://www.mymeteor.ie/mymeteorapi/index.cfm?event=smsAjax";
-		final ConnectionManager sendManager = new ConnectionManager(sendUrl);
-		sendManager.addPostHeader("ajaxRequest", "sendSMS");
-		sendManager.addPostHeader("messageText", message);
+		final ConnectionManager sendManager = new ConnectionManager(SMS_URL);
+		sendManager.addPostHeader(POST_AJAX_REQUEST, POST_VALUE_SEND_SMS);
+		sendManager.addPostHeader(POST_MESSAGE_TEXT, message);
+
 		return sendManager.doConnection();
+	}
+
+	private void addRecipient(final String recipient) {
+		final String addUrl = SMS_URL;
+		final ConnectionManager addManager = new ConnectionManager(addUrl);
+		addManager.addPostHeader(POST_AJAX_REQUEST, POST_VALUE_ADD_RECIPIENT);
+		addManager.addPostHeader(POST_REMOVE, POST_VALUE_NONE);
+		addManager.addPostHeader(POST_ADD, POST_VALUE_NO_ID + recipient);
+		addManager.doConnection();
 	}
 }
