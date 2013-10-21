daaae94a8bdc2c5c2b100755d47241ce25168633
Seán Dunne <sean.dunne@ericsson.com>
2013-10-21 15:45:33 +0100

    Added support for Contact suggestions .. Still needs some work

--- src/com/icc/view/ContactAdapter.java
+++ src/com/icc/view/ContactAdapter.java
@@ -2,7 +2,11 @@ package com.icc.view;
 
 import java.util.ArrayList;
 
+import android.content.ContentResolver;
 import android.content.Context;
+import android.database.Cursor;
+import android.net.Uri;
+import android.provider.ContactsContract.Contacts;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,18 +17,14 @@ import android.widget.TextView;
 
 public class ContactAdapter extends BaseAdapter implements Filterable {
 
-	private final String[] defaultItems = new String[] { "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight",
-			"Nine", "Ten", "Eleven", "Twleve", "Thirteen", "Fourteen", "Fiftheen", "Sixteen", "Seventeen", "Eighteen",
-			"Ninteen", "Twenty" };
 	private final LayoutInflater layoutInflater;
 	private ArrayList<String> items;
+	private final Context context;
 
 	public ContactAdapter(final Context context) {
+		this.context = context;
 		this.layoutInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
 		this.items = new ArrayList<String>();
-		for (final String s : this.defaultItems) {
-			this.items.add(s);
-		}
 	}
 
 	@Override
@@ -71,11 +71,16 @@ public class ContactAdapter extends BaseAdapter implements Filterable {
 
 			@Override
 			protected FilterResults performFiltering(final CharSequence constraint) {
+				final String[] PROJECTION = new String[] { Contacts._ID, Contacts.DISPLAY_NAME };
+				final Uri contentUri = Uri.withAppendedPath(Contacts.CONTENT_FILTER_URI, Uri.encode(constraint.toString()));
+
+				final ContentResolver resolver = ContactAdapter.this.context.getContentResolver();
+				final Cursor c = resolver.query(contentUri, PROJECTION, null, null, null);
+
 				final ArrayList<String> values = new ArrayList<String>();
-				for (final String value : ContactAdapter.this.items) {
-					if (value.contains(constraint)) {
-						values.add(value);
-					}
+				while (c.moveToNext()) {
+					final String cName = c.getString(1);
+					values.add(cName);
 				}
 
 				final FilterResults result = new FilterResults();
