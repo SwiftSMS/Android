a8a7701723bd4859473fa39352c16848aaacdebb
Seán Dunne <sean.dunne@ericsson.com>
2013-11-15 11:56:29 +0000

    ref #32 Cleaned up ConnectionManager added support for a POST request body

--- src/com/icc/net/ConnectionManager.java
+++ src/com/icc/net/ConnectionManager.java
@@ -9,17 +9,19 @@ import java.io.OutputStreamWriter;
 import java.io.Writer;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.net.URLConnection;
 import java.util.LinkedHashMap;
-import java.util.List;
 import java.util.Map;
 
 public class ConnectionManager {
 
-	private HttpURLConnection connection;
-	private String webpageUrl;
-	private final Map<String, String> requestHeaders = new LinkedHashMap<String, String>();
 	private final boolean doOutput;
 	private final String method;
+
+	private HttpURLConnection connection;
+	private String webpageUrl;
+	private Map<String, String> requestHeaders;
+	private StringBuilder requestOutput;
 
 	/**
 	 * Create a new ConnectionManager with the settings provided to open a connection to the URL provided using the
@@ -54,6 +56,8 @@ public class ConnectionManager {
 	private void initalize() {
 		try {
 			final URL url = new URL(this.webpageUrl);
+			this.requestHeaders = new LinkedHashMap<String, String>();
+			this.requestOutput = new StringBuilder();
 			this.connection = (HttpURLConnection) url.openConnection();
 			// this.connection.setChunkedStreamingMode(0);
 			this.connection.setRequestMethod(this.method);
@@ -64,10 +68,36 @@ public class ConnectionManager {
 		}
 	}
 
+	/**
+	 * This method adds a HTTP POST variable key-pair to this {@link URLConnection}.
+	 * 
+	 * @param key
+	 *            The key used for this POST header.
+	 * @param value
+	 *            The value to use for this POST header.
+	 */
 	public void addPostHeader(final String key, final String value) {
 		this.requestHeaders.put(key, value);
 	}
 
+	/**
+	 * This method is used to add text to a request. The data entered here is written in the request body.
+	 * 
+	 * @param text
+	 *            The text to write.
+	 */
+	public void addRequestBody(final String text) {
+		this.requestOutput.append(text);
+	}
+
+	/**
+	 * This method will set a request header on the {@link URLConnection}.
+	 * 
+	 * @param field
+	 *            The key used for this request header.
+	 * @param value
+	 *            The request header value to set.
+	 */
 	public void setRequestHeader(final String field, final String value) {
 		this.connection.setRequestProperty(field, value);
 	}
@@ -88,17 +118,14 @@ public class ConnectionManager {
 		try {
 			if (this.connection.getDoOutput()) {
 				final Writer writer = new BufferedWriter(new OutputStreamWriter(this.connection.getOutputStream()));
-				boolean firstHeader = true;
-				for (final String key : this.requestHeaders.keySet()) {
-					if (!firstHeader) {
-						writer.write("&");
-					}
-					writer.write(key + "=" + this.requestHeaders.get(key));
-					firstHeader = false;
+				if (this.requestHeaders.size() > 0) {
+					this.writeHeaders(writer);
+				}
+				if (this.requestOutput.length() > 0) {
+					writer.write(this.requestOutput.toString());
 				}
 				writer.close();
 			}
-
 			result.append(this.readStream(this.connection.getInputStream()));
 		} catch (final IOException e) {
 			// TODO Auto-generated catch block
@@ -107,8 +134,22 @@ public class ConnectionManager {
 		return result.toString();
 	}
 
-	public Map<String, List<String>> getResponseHeaders() {
-		return this.connection.getHeaderFields();
+	/**
+	 * This method is responsible for writing HTTP POST headers to a {@link URLConnection}'s output stream.
+	 * 
+	 * @param writer
+	 *            A writer wrapped around a {@link URLConnection}'s output stream.
+	 * @throws IOException
+	 */
+	private void writeHeaders(final Writer writer) throws IOException {
+		boolean firstHeader = true;
+		for (final String key : this.requestHeaders.keySet()) {
+			if (!firstHeader) {
+				writer.write("&");
+			}
+			writer.write(key + "=" + this.requestHeaders.get(key));
+			firstHeader = false;
+		}
 	}
 
 	public int getResponseStatus() {
@@ -119,16 +160,6 @@ public class ConnectionManager {
 			e.printStackTrace();
 		}
 		return -1;
-	}
-
-	public String getResponseMessage() {
-		try {
-			return this.connection.getResponseMessage();
-		} catch (final IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		return null;
 	}
 
 	private String readStream(final InputStream is) throws IOException {
