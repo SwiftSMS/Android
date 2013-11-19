6cbe0c9f628b4d4af6129ee73dd896f43d072942
Sean Dunne <sean.dunne1988@gmail.com>
2013-11-19 22:59:20 +0000

    JavaDoc and code formatting

--- src/com/icc/db/IAccountDatabase.java
+++ src/com/icc/db/IAccountDatabase.java
@@ -1,47 +1,47 @@
 package com.icc.db;
 
-import com.icc.model.Account;
-
 import java.util.List;
 
+import com.icc.model.Account;
+
 /**
- * Interface used to talk to the Account's Database
- *
+ * Interface used to talk to the Account's Database.
+ * 
  * @author Rob Powell
  */
 public interface IAccountDatabase {
 
-    /**
-     *
-     * @param account
-     *                  Account object to add to the Database
-     * @return
-     *         Success returns the new row id, failed returns -1
-     */
-    public int addAccount(final Account account);
+	/**
+	 * Add a new account to the application.
+	 * 
+	 * @param account
+	 *            Account object to add to the Database.
+	 * @return Success returns the new row id, failed returns -1.
+	 */
+	public int addAccount(final Account account);
 
-    /**
-     *
-     * @param account
-     *               Account object to remove from database, deleted by ID
-     * @return
-     *        True on Account deletion
-     */
-    public boolean removeAccount(final Account account);
+	/**
+	 * Remove an account from the application.
+	 * 
+	 * @param account
+	 *            Account object to remove from database, deleted by ID.
+	 * @return True on Account deletion.
+	 */
+	public boolean removeAccount(final Account account);
 
-    /**
-     *
-     * @return
-     *          All Account's from the Database
-     */
-    public List<Account> getAllAccounts();
+	/**
+	 * Get a list of all accounts currently in the application.
+	 * 
+	 * @return All Account's from the Database.
+	 */
+	public List<Account> getAllAccounts();
 
-    /**
-     *
-     * @param id
-     *          ID of the Account object you need from the database
-     * @return
-     *          The Account of the specified ID
-     */
-    public Account getAccountById(final int id);
+	/**
+	 * Get a stored account based on it's id.
+	 * 
+	 * @param id
+	 *            ID of the Account object you need from the database.
+	 * @return The Account of the specified ID.
+	 */
+	public Account getAccountById(final int id);
 }
