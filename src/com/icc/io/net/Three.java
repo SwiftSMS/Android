7b540e05d68a0681093358a22cd00b455a40354b
Rob Powell <rob.p.tec@gmail.com>
2013-12-17 17:01:25 +0000

    Three login/sending sms working :)

--- src/com/icc/io/net/Three.java
+++ src/com/icc/io/net/Three.java
@@ -1,5 +1,6 @@
 package com.icc.io.net;
 
+import java.util.Iterator;
 import java.util.List;
 
 import com.icc.model.Account;
@@ -9,34 +10,79 @@ import com.icc.model.Account;
  */
 public class Three extends Operator {
 
+    // login strings/ values
+    private static final String LOGIN_URL = "https://webtexts.three.ie/webtext/users/login";
+    private static final String POST_USER = "data[User][telephoneNo]";
+    private static final String POST_PASS = "data[User][pin]";
+    private static final String SUCCESS_LOGIN = "Logout";
+
+    // sms related strings values
+    private static final String POST_SMS_URL = "https://webtexts.three.ie/webtext/messages/send";
+    private static final String POST_MESSAGE_TEXT = "data[Message][message]";
+    private static final String POST_RECIPIENT_INDIVIDUAL = "data[Message][recipients_individual]";
+    private static final String SMS_SEND_SUCCESS_TEXT = "Message sent!";
+
+    // contact/recipients
+    private static final String RECIPIENTS_SEPARATOR = ",";
+
+    //
+    private static final String GET_REQUEST_METHOD = "GET";
+
+
 	public Three(final Account account) {
 		super(account);
 	}
 
 	@Override
 	boolean doLogin() {
-		final ConnectionManager loginManager = new ConnectionManager("https://webtexts.three.ie/webtext/users/login");
-		loginManager.addPostHeader("UserTelephoneNo", this.getAccount().getMobileNumber());
-		loginManager.addPostHeader("UserPin", this.getAccount().getPassword());
+		final ConnectionManager loginManager = new ConnectionManager(Three.LOGIN_URL);
+		loginManager.addPostHeader(Three.POST_USER, this.getAccount().getMobileNumber());
+		loginManager.addPostHeader(Three.POST_PASS, this.getAccount().getPassword());
 		final String loginHtml = loginManager.connect();
 
-		return loginHtml.contains("Logout");
+		return loginHtml.contains(SUCCESS_LOGIN);
 	}
 
 	@Override
 	boolean doSend(final List<String> recipients, final String message) {
-		// TODO Auto-generated method stub
-		return false;
+
+        final ConnectionManager smsSendManager = new ConnectionManager(Three.POST_SMS_URL);
+
+        smsSendManager.addPostHeader(Three.POST_RECIPIENT_INDIVIDUAL, parseRecipients(recipients));
+        smsSendManager.addPostHeader(Three.POST_MESSAGE_TEXT, message);
+        final String html = smsSendManager.connect();
+
+        return html.contains(Three.SMS_SEND_SUCCESS_TEXT);
 	}
 
-	@Override
+    private String parseRecipients(List<String> recipients) {
+
+        String recipientsString = "";
+
+        Iterator<String> recipientIterator = recipients.iterator();
+
+        while(recipientIterator.hasNext()) {
+            recipientsString += recipientIterator.next();
+
+            if(recipientIterator.hasNext())
+                recipientsString += RECIPIENTS_SEPARATOR;
+        }
+        return recipientsString;
+    }
+
+    @Override
 	int doGetRemainingSMS() {
-		// TODO Auto-generated method stub
+
+        final ConnectionManager manager = new ConnectionManager(POST_SMS_URL,
+                Three.GET_REQUEST_METHOD, false);
+
+        final String html = manager.connect();
+
 		return 0;
 	}
 
 	@Override
 	int doGetCharacterLimit() {
 		return 160;
-	}
+    }
 }
