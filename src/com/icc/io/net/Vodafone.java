3b940951c30ad883a95a25ec7da2ceb6e94b84ff
Sean Dunne <sean.dunne1988@gmail.com>
2013-12-19 11:50:25 +0000

    Fixed Vodafone (Party timegit commit --amend --reset-author)

--- src/com/icc/io/net/Vodafone.java
+++ src/com/icc/io/net/Vodafone.java
@@ -1,5 +1,9 @@
 package com.icc.io.net;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.List;
 import java.util.concurrent.Semaphore;
 
@@ -10,78 +14,72 @@ import android.app.AlertDialog;
 import android.app.AlertDialog.Builder;
 import android.content.Context;
 import android.content.DialogInterface;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
-import android.util.Log;
+import android.os.Handler;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
 
-import com.icc.InternalString;
 import com.icc.R;
 import com.icc.model.Account;
-import com.icc.tasks.DownloadImageTask;
 
 public class Vodafone extends Operator {
 
-	private final Semaphore lock = new Semaphore(1);;
+	private static final String CHARS_URL = "https://www.vodafone.ie/javascript/section.myv.webtext.js";
+	private static final String TOKEN_URL = "https://www.vodafone.ie/myv/messaging/webtext/index.jsp";
+	private static final String CAPTCHA_URL = "https://www.vodafone.ie/myv/messaging/webtext/Challenge.shtml";
+
+	private static final String SMS_JSON_USED = "used";
+	private static final String SMS_JSON_TOTAL = "total";
+	private static final String SMS_URL = "https://www.vodafone.ie/myv/dashboard/webtextdetails.shtml";
+
+	private static final String SEND_SUCCESS_STRING = "Message sent!";
+	private static final String SEND_POST_CAPTCHA = "jcaptcha_response";
+	private static final String SEND_POST_MESSAGE = "message";
+	private static final String SEND_POST_TOKEN = "org.apache.struts.taglib.html.TOKEN";
+	private static final String SEND_URL = "https://www.vodafone.ie/myv/messaging/webtext/Process.shtml";
+
+	private static final String LOGIN_SUCCESS_STRING = "Sign out";
+	private static final String LOGIN_POST_PASSWORD = "password";
+	private static final String LOGIN_POST_USERNAME = "username";
+	private static final String LOGIN_URL = "https://www.vodafone.ie/myv/services/login/Login.shtml";
+
+	private final Semaphore lock = new Semaphore(1);
 	private EditText answerEditText;
+	private ImageView imageView;
+	private Handler handler;
 
 	public Vodafone(final Account account) {
 		super(account);
 	}
 
-	/**
-	 * <pre>
-	 * Steps to send
-	 * 	1. Get session cookie
-	 * 		- https://www.vodafone.ie/myv/services/login/Login.shtml
-	 * 		  POST
-	 * 			username=phone
-	 * 			password=pin
-	 * 		- Session cookie will be in the returned headers
-	 * 		- Form to replicate logging in
-	 * 			form/vodafone_login.html
-	 * 	2. Send message
-	 * 		- https://www.vodafone.ie/myv/messaging/webtext/Process.shtml
-	 * 		  POST
-	 * 			org.apache.struts.taglib.html.TOKEN=xxxx
-	 * 			message=My Message
-	 * 			recipients[0]=NUMBER
-	 * 			recipients[1]=NUMBER
-	 * 			recipients[2]=NUMBER
-	 * 			recipients[3]=NUMBER
-	 * 			recipients[4]=NUMBER
-	 * 			jcaptcha_response=CAPTCHA
-	 * 		- Form to replicate the adding
-	 * 			form/vodafone_send.html
-	 * </pre>
-	 */
-
 	@Override
 	boolean doLogin() {
-		final ConnectionManager loginManager = new ConnectionManager("https://www.vodafone.ie/myv/services/login/Login.shtml");
-		loginManager.addPostHeader("username", this.getAccount().getMobileNumber());
-		loginManager.addPostHeader("password", this.getAccount().getPassword());
+		final ConnectionManager loginManager = new ConnectionManager(LOGIN_URL);
+		loginManager.addPostHeader(LOGIN_POST_USERNAME, this.getAccount().getMobileNumber());
+		loginManager.addPostHeader(LOGIN_POST_PASSWORD, this.getAccount().getPassword());
 		final String loginHtml = loginManager.connect();
 
-		return loginHtml.contains("Sign out");
+		return loginHtml.contains(LOGIN_SUCCESS_STRING);
 	}
 
 	@Override
 	public void preSend(final Context context) {
 		this.lock.acquireUninterruptibly();
+		this.handler = new Handler(context.getMainLooper());
 
 		final LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
 		final View layout = inflater.inflate(R.layout.captcha_dialog, null);
 		this.answerEditText = (EditText) layout.findViewById(R.id.text_captcha_dialog);
-		final ImageView imageView = (ImageView) layout.findViewById(R.id.image_captcha_dialog);
-		new DownloadImageTask(imageView).execute("https://www.vodafone.ie/myv/messaging/webtext/Challenge.shtml");
+		this.imageView = (ImageView) layout.findViewById(R.id.image_captcha_dialog);
 
 		final Builder dialog = new AlertDialog.Builder(context);
-		dialog.setTitle("Captcha");
+		dialog.setTitle(R.string.captcha);
 		dialog.setView(layout);
-		dialog.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+		dialog.setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
 			@Override
 			public void onClick(final DialogInterface dialog, final int which) {
 				dialog.dismiss();
@@ -93,34 +91,45 @@ public class Vodafone extends Operator {
 
 	@Override
 	boolean doSend(final List<String> recipients, final String message) {
-		Log.d(InternalString.LOG_TAG, "doSend - wait for captcha dialog");
-		this.lock.acquireUninterruptibly();
-		Log.d(InternalString.LOG_TAG, "doSend - captcha dialog done: " + this.answerEditText.getText().toString());
-		Log.d(InternalString.LOG_TAG, "doSend - get TOKEN");
 		final String token = this.getToken();
-		Log.d(InternalString.LOG_TAG, "doSend - TOKEN generated: " + token);
-		final ConnectionManager manager = new ConnectionManager("https://www.vodafone.ie/myv/messaging/webtext/Process.shtml");
-		manager.addPostHeader("org.apache.struts.taglib.html.TOKEN", token);
-		Log.d(InternalString.LOG_TAG, "doSend - message: " + Uri.encode(message));
-		manager.addPostHeader("message", Uri.encode(message));
+		this.downloadCaptcha();
+		this.lock.acquireUninterruptibly(); // wait here for the user to solve the captcha
+
+		final ConnectionManager manager = new ConnectionManager(SEND_URL);
+		manager.addPostHeader(SEND_POST_TOKEN, token);
+		manager.addPostHeader(SEND_POST_MESSAGE, Uri.encode(message));
 		for (int i = 0; i < recipients.size(); i++) {
 			manager.addPostHeader(Uri.encode("recipients[" + i + "]"), Uri.encode(recipients.get(i)));
 		}
-		manager.addPostHeader("jcaptcha_response", this.answerEditText.getText().toString());
-		Log.d(InternalString.LOG_TAG, "doSend - try send SMS");
+		manager.addPostHeader(SEND_POST_CAPTCHA, this.answerEditText.getText().toString());
 		final String html = manager.connect();
-		Log.d(InternalString.LOG_TAG, "doSend - SMS sent:");
-		Log.d(InternalString.LOG_TAG, html);
-
-		Log.d(InternalString.LOG_TAG, "doSend - send Response Status: " + manager.getResponseStatus());
 
 		this.lock.release();
-		return html.contains("Message sent!");
+		return html.contains(SEND_SUCCESS_STRING);
+	}
+
+	private void downloadCaptcha() {
+		try {
+			final InputStream is = new URL(CAPTCHA_URL).openStream();
+			final Bitmap image = BitmapFactory.decodeStream(is);
+
+			this.handler.post(new Runnable() {
+				@Override
+				public void run() {
+					Vodafone.this.imageView.setImageBitmap(image);
+				}
+			});
+		} catch (final MalformedURLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (final IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 	private String getToken() {
-		final ConnectionManager manager = new ConnectionManager("https://www.vodafone.ie/myv/messaging/webtext/index.jsp",
-				"GET", false);
+		final ConnectionManager manager = new ConnectionManager(TOKEN_URL, "GET", false);
 		final String html = manager.connect();
 
 		final String charsText = "org.apache.struts.taglib.html.TOKEN\" value=\"";
@@ -134,14 +143,13 @@ public class Vodafone extends Operator {
 
 	@Override
 	int doGetRemainingSMS() {
-		final ConnectionManager manager = new ConnectionManager("https://www.vodafone.ie/myv/dashboard/webtextdetails.shtml",
-				"GET", false);
+		final ConnectionManager manager = new ConnectionManager(SMS_URL, "GET", false);
 		final String smsHtml = manager.connect();
 
 		try {
 			final JSONObject smsJson = new JSONObject(smsHtml);
-			final int totalSms = smsJson.getInt("total");
-			final int usedSms = smsJson.getInt("used");
+			final int totalSms = smsJson.getInt(SMS_JSON_TOTAL);
+			final int usedSms = smsJson.getInt(SMS_JSON_USED);
 			return totalSms - usedSms;
 		} catch (final JSONException e) {
 			// TODO Auto-generated catch block
@@ -152,8 +160,7 @@ public class Vodafone extends Operator {
 
 	@Override
 	int doGetCharacterLimit() {
-		final ConnectionManager manager = new ConnectionManager("https://www.vodafone.ie/javascript/section.myv.webtext.js",
-				"GET", false);
+		final ConnectionManager manager = new ConnectionManager(CHARS_URL, "GET", false);
 		final String html = manager.connect();
 
 		final String charsText = "var char_limit = ";
