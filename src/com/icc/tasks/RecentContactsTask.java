a9602a29e5807b062d8ec038050ddc3975917bfe
Seán Dunne <sean.dunne1988@gmail.com>
2014-01-09 09:07:33 +0000

    Finished the Recent Contacts query, now displays Recent Contacts

--- src/com/icc/tasks/RecentContactsTask.java
+++ src/com/icc/tasks/RecentContactsTask.java
@@ -6,16 +6,13 @@ import java.util.List;
 import android.content.ContentResolver;
 import android.content.Context;
 import android.database.Cursor;
-import android.net.Uri;
 import android.os.AsyncTask;
 import android.provider.ContactsContract.CommonDataKinds.Phone;
 import android.provider.ContactsContract.Contacts;
-import android.provider.ContactsContract.Data;
 import android.widget.ListView;
 
 import com.icc.model.Contact;
 import com.icc.ui.view.util.BaseContactAdapter;
-import com.icc.utils.ContactUtils;
 
 public class RecentContactsTask extends AsyncTask<String, Integer, List<Contact>> {
 
@@ -29,25 +26,19 @@ public class RecentContactsTask extends AsyncTask<String, Integer, List<Contact>
 
 	@Override
 	protected List<Contact> doInBackground(final String... arg0) {
-		final String[] projection = new String[] { Contacts.DISPLAY_NAME, Contacts.PHOTO_THUMBNAIL_URI, Phone.NUMBER, Phone.TYPE, Phone.LABEL };
-		final String selection = Data.MIMETYPE + " = ?";
-		final String[] selectionArgs = new String[] { Phone.CONTENT_ITEM_TYPE };
-		final String sortOrder = Contacts.SORT_KEY_PRIMARY;
-
-		final String searchText = ContactUtils.getLastContact("Marguerite");
-		final Uri contentUri = Uri.withAppendedPath(Phone.CONTENT_FILTER_URI, Uri.encode(searchText));
+		final String[] projection = new String[] { Contacts.DISPLAY_NAME, Phone.NUMBER };
+		final String sortOrder = String.format("%s DESC LIMIT 5", Contacts.LAST_TIME_CONTACTED);
 
 		final ContentResolver resolver = this.context.getContentResolver();
-		final Cursor c = resolver.query(contentUri, projection, selection, selectionArgs, sortOrder);
+		final Cursor c = resolver.query(Phone.CONTENT_URI, projection, null, null, sortOrder);
 
 		final ArrayList<Contact> values = new ArrayList<Contact>();
 		while (c.moveToNext()) {
 			final String cName = c.getString(0);
-			final String cNumber = c.getString(2);
+			final String cNumber = c.getString(1);
 
 			values.add(new Contact(cName, "", cNumber, null));
 		}
-
 		return values;
 	}
 
