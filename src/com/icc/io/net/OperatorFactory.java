f7b80f023c848feaa72eed08113f01966736f7dc
Seán Dunne <sean.dunne@ericsson.com>
2014-01-15 09:29:35 +0000

    Removed O2 & EMobile from the App

--- src/com/icc/io/net/OperatorFactory.java
+++ src/com/icc/io/net/OperatorFactory.java
@@ -6,12 +6,12 @@ public class OperatorFactory {
 
 	public static Operator getOperator(final Account account) {
 		switch (account.getOperator()) {
-		case EMOBILE:
-			return null;
+		// case EMOBILE:
+		// return null;
 		case METEOR:
 			return new Meteor(account);
-		case O2:
-			return new O2(account);
+			// case O2:
+			// return new O2(account);
 		case TESCO:
 			return new Tesco(account);
 		case THREE:
