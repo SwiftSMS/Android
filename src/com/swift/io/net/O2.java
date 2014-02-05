d7f497e51e831e6068dc2d40194b1a31b9c6e751
Sean Dunne <sean.dunne1988@gmail.com>
2014-02-05 22:43:33 +0000

    More messing with O2, got nothing

--- src/com/swift/io/net/O2.java
+++ src/com/swift/io/net/O2.java
@@ -10,6 +10,8 @@ import com.swift.utils.HTMLParser;
 
 public class O2 extends Operator {
 
+	private String preWebtextUrl;
+
 	public O2(final Account account) {
 		super(account);
 	}
@@ -18,56 +20,63 @@ public class O2 extends Operator {
 	boolean doLogin() {
 		final ConnectionManager preMgr = new ConnectionManager("https://www.o2online.ie/o2/my-o2/", "GET", false);
 		preMgr.setRequestHeader("User-Agent", "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:26.0) Gecko/20100101 Firefox/26.0");
-		preMgr.setRequestHeader("Referer", "http://www.o2online.ie/o2/my-o2/");
+		preMgr.setRequestHeader("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8");
+		preMgr.setRequestHeader("Accept-Language", "en-gb,en;q=0.5");
+		preMgr.setRequestHeader("Accept-Encoding", "gzip, deflate");
 		final String preHtml = preMgr.connect();
-		preHtml.contains("Welcome to My O2");
+
+		final String requestId = HTMLParser.parseHtml(preHtml, "request_id\" value=\"", "\">");
 
 		final ConnectionManager manager = new ConnectionManager("https://www.o2online.ie/oam/server/auth_cred_submit");
 		manager.setRequestHeader("User-Agent", "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:26.0) Gecko/20100101 Firefox/26.0");
+		manager.setRequestHeader("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8");
+		manager.setRequestHeader("Accept-Language", "en-gb,en;q=0.5");
+		manager.setRequestHeader("Accept-Encoding", "gzip, deflate");
 		manager.addPostHeader("username", this.getAccount().getMobileNumber());
 		manager.addPostHeader("password", this.getAccount().getPassword());
-		manager.addPostHeader("request_id", this.getRequestId());
+		manager.addPostHeader("request_id", requestId);
 		final String html = manager.connect();
 
-		return html.contains("Redirect") && html.contains("o2online");
-	}
+		this.preWebtextUrl = HTMLParser.parseHtml(html, "title=\"Send a Webtext\" href=\"", "\">");
 
-	private String getRequestId() {
-		final ConnectionManager manager = new ConnectionManager("https://www.o2online.ie/idm/login/redirect.jsp", "GET", false);
-		final String html = manager.connect();
-
-		final String prefix = "request_id\" value=\"";
-		final String postfix = "\">";
-		return HTMLParser.parseHtml(html, prefix, postfix);
+		return html.contains("Welcome to My O2");
 	}
 
 	@Override
 	int doGetRemainingSMS() {
-		final String webtextUrl = "http://messaging.o2online.ie/" + this.getWebtextUrl();
-
-		final ConnectionManager manager = new ConnectionManager(webtextUrl, "GET", false);
-		final String html = manager.connect();
-
-		final String prefix = "spn_WebtextFree\">";
-		final String postfix = "</span>";
-		final String remainingSMS = HTMLParser.parseHtml(html, prefix, postfix);
-
 		int remainingSMSCount = -1;
-		if (remainingSMS != null) {
-			remainingSMSCount = Integer.parseInt(remainingSMS);
+		if (this.preWebtextUrl != null) {
+			final ConnectionManager manager = new ConnectionManager(this.getWebtextUrl(), "GET", false);
+			manager.setRequestHeader("User-Agent", "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:26.0) Gecko/20100101 Firefox/26.0");
+			manager.setRequestHeader("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8");
+			manager.setRequestHeader("Referer", this.preWebtextUrl);
+			manager.setRequestHeader("Accept-Language", "en-gb,en;q=0.5");
+			manager.setRequestHeader("Accept-Encoding", "gzip, deflate");
+			final String html = manager.connect();
+
+			final String prefix = "spn_WebtextFree\">";
+			final String postfix = "</span>";
+			final String remainingSMS = HTMLParser.parseHtml(html, prefix, postfix);
+
+			if (remainingSMS != null) {
+				remainingSMSCount = Integer.parseInt(remainingSMS);
+			}
 		}
 		return remainingSMSCount;
 	}
 
 	private String getWebtextUrl() {
-		final ConnectionManager manager = new ConnectionManager(
-				"http://messaging.o2online.ie/ssomanager.osp?APIID=AUTH-WEBSSO&TargetApp=o2om_smscenter_new.osp%3FMsgContentID%3D-1%26SID%3D_", "GET", false);
+		final ConnectionManager manager = new ConnectionManager(this.preWebtextUrl, "GET", false);
+		manager.setRequestHeader("User-Agent", "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:26.0) Gecko/20100101 Firefox/26.0");
+		manager.setRequestHeader("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8");
+		manager.setRequestHeader("Accept-Language", "en-gb,en;q=0.5");
+		manager.setRequestHeader("Accept-Encoding", "gzip, deflate");
 		final String html = manager.connect();
 
 		final String prefix = "name=\"frame_content\" src=\"";
 		final String postfix = "\"";
 
-		return HTMLParser.parseHtml(html, prefix, postfix);
+		return "http://messaging.o2online.ie/" + HTMLParser.parseHtml(html, prefix, postfix);
 	}
 
 	@Override
