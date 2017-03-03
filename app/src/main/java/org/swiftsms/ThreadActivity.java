f7386957cfed5f1bf0b79c47757b1a67d3f3051b
Seán Dunne <sean.dunne1988@gmail.com>
2017-03-03 20:22:51 +0000

    Generate conversation history activity

---
+++ app/src/main/java/org/swiftsms/ThreadActivity.java
@@ -0,0 +1,13 @@
+package org.swiftsms;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ThreadActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_thread);
+    }
+}
