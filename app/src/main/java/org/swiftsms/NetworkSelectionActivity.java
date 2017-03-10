8c1f71e45e7a164b9f8425819ce5b66c75cf5e10
Seán Dunne <sean.dunne1988@gmail.com>
2017-03-10 22:12:25 +0000

    Generate login activity

--- app/src/main/java/org/swiftsms/NetworkSelectionActivity.java
+++ app/src/main/java/org/swiftsms/NetworkSelectionActivity.java
@@ -1,12 +1,20 @@
 package org.swiftsms;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.GridView;
 
+import org.swiftsms.models.Network;
 import org.swiftsms.views.adapters.NetworkSelectionAdapter;
 
 public class NetworkSelectionActivity extends AppCompatActivity {
+
+    private static final int ADD_ACCOUNT_REQUEST_CODE = 456;
+    private static final String OPERATOR = "operator";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,5 +24,33 @@ public class NetworkSelectionActivity extends AppCompatActivity {
 
         final GridView gridView = (GridView) findViewById(R.id.networks);
         gridView.setAdapter(new NetworkSelectionAdapter(this));
+        gridView.setOnItemClickListener(new NetworkOnItemClickListener(this));
+    }
+
+    @Override
+    protected void onActivityResult(final int requestCode, final int resultCode, final Intent data) {
+        if (requestCode == ADD_ACCOUNT_REQUEST_CODE) {
+            if (resultCode == RESULT_OK) {
+                this.setResult(RESULT_OK);
+                this.finish();
+            }
+        }
+    }
+
+    private static class NetworkOnItemClickListener implements AdapterView.OnItemClickListener {
+
+        private final Activity context;
+
+        public NetworkOnItemClickListener(Activity context) {
+            this.context = context;
+        }
+
+        @Override
+        public void onItemClick(final AdapterView<?> parent, final View view, final int position, final long id) {
+            final Intent intent = new Intent(context, AddAccountActivity.class);
+            final Network network = (Network) parent.getItemAtPosition(position);
+            intent.putExtra(OPERATOR, network.ordinal());
+            context.startActivityForResult(intent, ADD_ACCOUNT_REQUEST_CODE);
+        }
     }
 }
